9e6170da97bab7fe954a2867aeb88a91fb238bc3
S.Zhang <zengeryounke@hotmail.com>
2024-07-03 16:18:24 -0700

    render session

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "./context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* wrap with Provide can get access the session data in client side */}
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import User from "./component/user";
 
-export default function Home() {
+/** The function need to wait the login callback, so it ill be an async function */
+export default async function Home() {
+  /** get the session data to show */
+  const session = await getServerSession(authOptions);
+
   return (
     <section>
       <h1>home</h1>
+      <h1>Server side Rendered</h1>
+      {/* show the callback session data */}
+      <pre>{JSON.stringify(session)}</pre>
+      <h1>Render in Client Side</h1>
+      <User />
     </section>
   );
 }
