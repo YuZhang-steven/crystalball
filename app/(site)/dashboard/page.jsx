9e1d35eba0e552dfd5989e2b8f6e29e3f2477eaa
S.Zhang <zengeryounke@hotmail.com>
2024-07-06 13:35:59 -0700

    add canvas

--- app/(site)/dashboard/page.jsx
+++ app/(site)/dashboard/page.jsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { Canvas } from "@react-three/fiber";
 import { useSession, signOut } from "next-auth/react";
 
 const dashboard = () => {
   const { data: session } = useSession();
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome {session?.user?.email}</p>
-      <button onClick={() => signOut()}>Sign Out</button>
+    <div className="@apply fixed w-full h-full overflow-hidden left-0 top-0">
+      <div className=" relative flex flex-row justify-between">
+        <h1>Dashboard</h1>
+        <p>Welcome {session?.user?.email}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+
+      <Canvas>
+        <color
+          attach="background"
+          args={["#73d9e1"]}></color>
+      </Canvas>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,8 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/forms")],
+
+  corePlugins: {
+    preflight: true,
+  },
 };
