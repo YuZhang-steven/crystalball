37c5fbb603dac599968e2f58bbaa19ab16f4f68a
S.Zhang <zengeryounke@hotmail.com>
2024-08-04 17:19:04 -0700

    add client tpage

--- app/page.js
+++ app/page.js
@@ -1,20 +1,21 @@
+"use client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import User from "./component/User";
 
 /** The function need to wait the login callback, so it ill be an async function */
 export default async function Home() {
-  /** get the session data to show */
-  const session = await getServerSession(authOptions);
+	/** get the session data to show */
+	const session = await getServerSession(authOptions);
 
-  return (
-    <section>
-      <h1>home</h1>
-      <h1>Server side Rendered</h1>
-      {/* show the callback session data */}
-      <pre>{JSON.stringify(session)}</pre>
-      <h1>Render in Client Side</h1>
-      <User />
-    </section>
-  );
+	return (
+		<section>
+			<h1>home</h1>
+			<h1>Server side Rendered</h1>
+			{/* show the callback session data */}
+			<pre>{JSON.stringify(session)}</pre>
+			<h1>Render in Client Side</h1>
+			<User />
+		</section>
+	);
 }
