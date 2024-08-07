fc1052cc26b46f2649ed350774ce3ce6fc7c235d
S.Zhang <zengeryounke@hotmail.com>
2024-08-07 01:11:01 -0700

    input window frame

--- app/component/InputWind.jsx
+++ app/component/InputWind.jsx
@@ -10,12 +10,53 @@ export const Inputpopup = create((set) => ({
 
 export default function InputWind() {
 	const { openInput, closeInput } = Inputpopup();
+	function handleSubmit(formDate) {
+		event.preventDefault();
+		console.log("submit", formDate);
+		closeInput();
+	}
+
 	return (
-		<div className="fixed inset-0 m-auto z-10  bg-red-300  w-80 h-80">
-			<div className="flex flex-row min-h-fit justify-end bg-green-200 pr-2">
+		<div className="fixed inset-0 m-auto z-10  bg-gray-50 shadow-sm  w-80 h-fit pb-10 rounded-md">
+			<div className="flex flex-row min-h-fit rounded-t-md justify-end bg-gray-100 pr-2">
 				<button onClick={closeInput}>X</button>
 			</div>
-			InputWind
+			<form className="space-y-6 bg" onSubmit={handleSubmit}>
+				<div className="mt-2 mx-2">
+					<label
+						htmlFor="birthday"
+						className="block text-m font-medium leading-10 text-gray-900">
+						Birthday
+					</label>
+					<input
+						requuired
+						id="birthday"
+						name="birthday"
+						type="date"
+						autoComplete="bday"
+						className="block w-full py-1.5 rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-300"
+					/>
+				</div>
+				<div className="mx-2">
+					<label
+						htmlFor="birthday"
+						className="block text-m font-medium leading-10 text-gray-900">
+						What do you want to know?
+					</label>
+					<input
+						requuired
+						type="text"
+						name="question"
+						className="block w-full py-1.5 rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-300"
+						placeholder="Enter your question here"
+					/>
+				</div>
+				<button
+					type="submit"
+					className="flex w-fit justify-center mx-auto px-3 rounded-md bg-white font-semibold ">
+					Submit
+				</button>
+			</form>
 		</div>
 	);
 }
